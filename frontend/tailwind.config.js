cb15d0081b3fd890922211368b0160136c2794f0
JasimDAce <mohdalijasim786@gmail.com>
2024-10-26 14:09:49 +0530

    fixed the screen issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
   },
   theme: {
     extend: {
+      screens:{
+        'xs' : {'max': '639px'}
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
